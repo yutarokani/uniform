514eee0e7a440e3c9e243cbff9f8b06d3cc3bdf1
yutarokani <y-kani@sr-net.co.jp>
2024-06-19 13:49:20 +0900

    Add files via upload

--- bean/OrderDetail.java
+++ bean/OrderDetail.java
@@ -2,16 +2,45 @@ package bean;
 
 public class OrderDetail {
 
-	private String uniId;
-	
-	private int orderNumber;
+	private int orderNumber;//注文番号
+	private String name;//購入者氏名
+	private String mail;//購入者メールアドレス
+	private String uniId;//商品名
+	private int buyQuantity;//購入個数
+	private String day;//注文日
 
-	private int buyQuantity;
+	public OrderDetail() {
 
-	public orderDetail() {
-		this.uniId = null;
 		this.orderNumber = 0;
+		this.name = null;
+		this.mail = null;
+		this.uniId = null;
 		this.buyQuantity = 0;
+		this.day = null;
+	}
+
+	public int getOrderNumber() {
+		return orderNumber;
+	}
+
+	public void setOrderNumber(int orderNumber) {
+		this.orderNumber = orderNumber;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
 	}
 
 	public String getUniId() {
@@ -21,22 +50,20 @@ public class OrderDetail {
 	public void setUniId(String uniId) {
 		this.uniId = uniId;
 	}
-	
-	public int getOrderNumber() {
-		return orderNumber;
-	}
 
-	public void setOrderNumber(int orderNumber) {
-		this.orderNumber = orderNumber;
-	}
-	
-		public int getBuyQuantity() {
+	public int getBuyQuantity() {
 		return buyQuantity;
 	}
 
 	public void setBuyQuantity(int buyQuantity) {
 		this.buyQuantity = buyQuantity;
 	}
-}
 
-	
+	public String getDay() {
+		return day;
+	}
+
+	public void setDay(String day) {
+		this.day = day;
+	}
+}
