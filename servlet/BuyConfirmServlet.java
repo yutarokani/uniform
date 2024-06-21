89e41e6d94fb648e028853c83c35d571030df10a
yutarokani <y-kani@sr-net.co.jp>
2024-06-21 11:04:30 +0900

    Add files via upload

--- servlet/BuyConfirmServlet.java
+++ servlet/BuyConfirmServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import bean.Admin;
+import bean.Goods;
 import bean.OrderInfo;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -11,6 +12,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import util.SendMail;
 
 @WebServlet("/buyConfirm")
 public class BuyConfirmServlet extends HttpServlet {
@@ -24,7 +26,12 @@ public class BuyConfirmServlet extends HttpServlet {
 		String ercmd = null;
 		
 		try {
-						
+			
+			OrderInfo orderedinfo = new OrderInfo();
+			
+			// 送信メールで表示する購入商品を格納するリスト
+			ArrayList<Goods> mail_list = new ArrayList<Goods>();
+
 			// セッションから"admin"を取得する。(セッション切れの場合はerror.jspに遷移する)
 			HttpSession session = request.getSession();
 			Admin admin = (Admin)session.getAttribute("admin");
@@ -39,7 +46,7 @@ public class BuyConfirmServlet extends HttpServlet {
 			 * セッションから、カートの中身を表す"order_list"を取得する。
 			 * (カートの中身がない場合はerror.jspに遷移する)
 			 */
-			ArrayList<OrderInfo> order_list = (ArrayList<OrderInfo>)session.getAttribute("order_list");
+			ArrayList<Goods> order_list = (ArrayList<Goods>)session.getAttribute("order_list");
 			
 			if(order_list.size() == 0) {
 				error = "カートの中に何もなかったので購入は出来ません。";
@@ -47,19 +54,42 @@ public class BuyConfirmServlet extends HttpServlet {
 				return;
 			}
 			
-			// Orderオブジェクトを格納する配列を作成
-			ArrayList<OrderInfo> order = new ArrayList<OrderInfo>();
+			// Goodsオブジェクトを格納する配列を作成
+			ArrayList<Goods> goods_list = new ArrayList<Goods>();
 			
 			// 関連メソッドをorder_listの(カート追加データ分）だけ呼び出す。
 			for(int i = 0;i < order_list.size();i++) {
-				OrderInfo addorder = order_list.get(i);
+				Goods add_goods = order_list.get(i);
 				
 				// 取得した各OrderをListに追加
-				order.add(addorder);
+				goods_list.add(add_goods);
 			}
 			
 			// リクエストスコープに"book_list"という名前で格納
-			request.setAttribute("deforder",order);
+			request.setAttribute("goods_list",goods_list);
+			
+			// "order_list"の注文情報内容をメール送信する。
+			SendMail sendmail = new SendMail();
+			/* 購入文章の記載、兼データの格納 */
+            String message1 = orderedinfo.getName() + "様\n\nユニフォームのご購入ありがとうございます。\n以下の内容でご注文を受け付けましたので、ご連絡致します。\n\n";
+            /* DAOメソッドの呼び出し、メソッドを利用して配列の値を格納 */
+            Goods sendGoods;
+            String message2 = ""; // null が入ってしまうため、空文字の挿入
+            for(int i = 0; i < order_list.size(); i++) {
+            	sendGoods = order_list.get(i);
+            	mail_list.add(sendGoods);
+            	
+            	/* 繰り返しから購入された書籍分のメッセージ準備 */
+				message2 += mail_list.get(i).getUniName() + "\t" + mail_list.get(i).getBuyQuantity() + "\n";
+            }
+
+            String message3 = "\n\nまたのご利用よろしくお願いします。";
+
+            /* 受け渡しデータの準備 */
+            String address = (String) orderedinfo.getMail(); // セッションからユーザーのEmail情報を取得
+            String mailMessage = message1 + message2 + message3; // メッセージの統合
+            
+            sendmail.Email(mailMessage, address, "ご購入ありがとうございます【神田】");
 			
 			// セッションの"order_list"情報をクリアする。
 			session.setAttribute("order_list",null);
---
+++ servlet/InsertServlet.java
@@ -0,0 +1,71 @@
+package servlet;
+
+import java.io.IOException;
+
+import bean.Goods;
+import dao.GoodsDAO;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/insert")
+public class InsertServlet extends HttpServlet {
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		//エラー処理先を決めるString型のerrorとcmdを宣言し初期化
+		String error = "";
+		String cmd = "list";
+		
+		GoodsDAO goodsDao = new GoodsDAO();
+		Goods goods = new Goods();
+		//コンテンツタイプ
+		response.setContentType("text/html;charset=UTF-8");
+		
+		String uniId = request.getParameter("uniId");
+
+		//各エラー処理
+		try {
+			Goods goods1 = goodsDao.selectByuniId(uniId);
+
+			if (request.getParameter("uniId").equals("")) {
+				error = "uniIdが未入力の為、書籍登録処理は行えませんでした。";
+			} else if (request.getParameter("uniName").equals("")) {
+				error = "uniNameが未入力の為、書籍登録処理は行えませんでした。";
+			} else if (request.getParameter("stock").equals("")) {
+				error = "stockが未入力の為、書籍登録処理は行えませんでした。";
+			} else if (request.getParameter("price").equals("")) {
+				error = "priceが未入力の為、書籍登録処理は行えませんでした。";
+			} else if (request.getParameter("uniId").equals(goods1.getUniId())){
+				error = "入力uniIdは既に登録済みの為、書籍登録処理は行えませんでした。";			
+			} else {
+				//getParameterで取得した情報をbookオブジェクトに格納
+				goods.setUniId(request.getParameter("uniId"));
+				goods.setUniName(request.getParameter("uniName"));
+				goods.setStock(Integer.parseInt(request.getParameter("stock")));
+				goods.setPrice(Integer.parseInt(request.getParameter("price")));
+				
+				goodsDao.insert(goods);
+			}
+				
+		} catch (IllegalStateException e) {
+			error = "DB接続エラーの為、書籍登録処理は行えませんでした";
+			cmd = "menu";
+		} catch (NumberFormatException e) {
+			error = "数値が不正の為、書籍登録処理は行えませんでした。";
+			cmd = "menu";
+
+		} finally {
+			if(error.equals("")) {
+				request.setAttribute("goods", goods);
+				request.getRequestDispatcher("/list").forward(request, response);
+			}else {
+				request.setAttribute("error", error);
+				request.setAttribute("cmd", cmd);
+				request.getRequestDispatcher("/view/error.jsp").forward(request, response);
+			}
+		}
+	}
+}
